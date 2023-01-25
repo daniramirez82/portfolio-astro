edb56b742403e91b6197623e61f4cf6af83bf6d9
daniramirez82 <dani.ramirez.madrid@gmail.com>
2023-01-25 07:38:02 +0100

    anchor working

--- src/components/HamMenu.jsx
+++ src/components/HamMenu.jsx
@@ -11,9 +11,8 @@ const HamMenu = ({ cates }) => {
     <div className="flex flex-col justify-end">
       <div
         onClick={handleClick}
-        className={`transition delay-200 duration-300 ease-in-out  z-50 rounded-md hover: w-14 p-1 flex justify-center self-end ${
-          isOpen ? "bg-slate-200" : ""
-        }`}
+        className={`transition delay-200 duration-300 ease-in-out  z-50 rounded-md hover: w-14 p-1 flex justify-center self-end ${isOpen ? "bg-slate-200" : ""
+          }`}
       >
         <svg
           className="w-14 fill-stone-900"
@@ -26,24 +25,23 @@ const HamMenu = ({ cates }) => {
       </div>
 
       <div
-        className={`absolute transition-all duration-500 right-0  z-30 rounded-md drop-shadow-md border bg-white border-amber-300 px-7 p-5 mr-4 mt-7 ${
-          isOpen ? "opacity-100 top-12" : "opacity-0 top-0"
-        }`}
+        className={`absolute transition-all duration-500 right-0  z-30 rounded-md drop-shadow-md border bg-white border-amber-300 px-7 p-5 mr-4 mt-7 ${isOpen ? "opacity-100 top-12" : "opacity-0 top-0"
+          }`}
       >
         <ul className="flex flex-col w-fit space-y-5">
-          {cates.map((i) => (
-            <li className="w-fit self-end " key={i}>
-              {i}
+          {cates.map(({ cate, link }) => (<a href={link} onClick={handleClick} className='cursor-pointer'>
+            <li className="w-fit self-end " key={cate}>
+              {cate}
             </li>
+          </a>
           ))}
         </ul>
       </div>
       {/* //backdrop */}
       <div
         onClick={handleClick}
-        className={`w-screen h-screen absolute left-0 top-0 z-10  ${
-          isOpen ? "" : "hidden"
-        }`}
+        className={`w-screen h-screen absolute left-0 top-0 z-10  ${isOpen ? "" : "hidden"
+          }`}
       ></div>
     </div>
   );
