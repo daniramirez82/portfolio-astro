7ea0532b8dccd8cd9707358b4c644e2f7c091ba6
daniramirez82 <dani.ramirez.madrid@gmail.com>
2023-01-28 17:30:09 +0100

    5

--- src/content/projects.js
+++ src/content/projects.js
@@ -18,13 +18,13 @@ export const projects = [
     demo: "#",
   },
   {
-    title: "SuPan",
-    image: "https://dummyimage.com/largerectangle",
-    cates: ["HTML", "CSS", "Svelte", "Git", "Heroku"],
-    description:
-      "Is an not so much UI thinking app developed in my last job in a backery, used for made request of bread from the stores to the workshop, and to plan all the bread producton includes the bread recipes and ingrediente managment. ",
-    code: "#",
-    demo: "#",
+    title: "Ciapp: Cafe Inventory App",
+    image: "/images/images-projects/ux-ui.png",
+    cates: ["Figma", "UX/UI"],
+    description: `This is a project framed within the context of the UX/UI Google Certification,
+     is a designed app in Figma complete with interactions and an established design system`,
+    code: "https://www.figma.com/file/2f6uObHKRwR5Eual0oSaSp/UX-Certficate?node-id=0%3A1&t=OjInpsKYhu9lPkTD-1",
+    demo: "https://www.figma.com/file/2f6uObHKRwR5Eual0oSaSp/UX-Certficate?node-id=0%3A1&t=OjInpsKYhu9lPkTD-1",
   },
   {
     title: "SuPan",
