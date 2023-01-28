33de9f2dbcc16013e99ca2ffac4c4f88690a41b5
Dani <dani.ramirez.madrid@gmail.com>
2023-01-28 19:28:18 +0100

    1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,41 @@
+import HamMenu from "./HamMenu";
+import { useState } from "preact/hooks";
+const cates =[{cate:'Home', link: '#Home'}, {cate:'Portfolio', link:'#Portfolio'}, {cate: 'About Me', link:'#About'}, {cate:'Contact', link: '#Contact'}];
+
+const Navbar = () => {
+  const [category, setCate] = useState("Home");
+
+  const handleClick = (e) => {
+    console.log(e.target.innerHTML);
+    setCate(e.target.innerHTML);
+  };
+  return (
+    <div className="fixed top-0 right-0 left-0 max-w-5xl mx-auto z-50 bg-white/80 backdrop-blur-sm h-10">
+      <div className="flex justify-between items-center relative z-50">
+        <div className="text-stone-800 font-extrabold text-4xl">DR</div>
+        <div className="md:hidden">
+          <HamMenu cates={cates} />
+        </div>
+        <div className="hidden md:block">
+          <ul className="flex">
+            {cates.map(({cate, link}) => {
+              return (
+                <a href={link}>
+                  <li
+                    onClick={handleClick}
+                    className={`pl-4 text-yellow-600 cursor-pointer ${cate === category ? "font-bold" : ""}`}
+                  >
+                    {cate}
+                  </li>
+                </a>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/content/projects.js
@@ -0,0 +1,38 @@
+export const projects = [
+  {
+    title: "The News",
+    image: "/images/images-projects/news.png",
+    cates: ["Nextjs", "Taildwind", "AtlassianUX", "Vercel"],
+    description:
+      "This is a personal project I used to learn Next.js. After learning a little bit of React, I proposed to myself to take advantage of the Next.js features, such as server-side rendering and lazy image loading. It is just a news site that takes the information from the New York Times API",
+    code: "https://github.com/daniramirez82/nextjs-blog",
+    demo: "http://nextjs-blog-daniramirez82.vercel.app/",
+  },
+  {
+    title: "SuPan",
+    image: "https://dummyimage.com/largerectangle",
+    cates: ["HTML", "CSS", "Svelte", "Git", "Heroku"],
+    description:
+      "Is an not so much UI thinking app developed in my last job in a backery, used for made request of bread from the stores to the workshop, and to plan all the bread producton includes the bread recipes and ingrediente managment. ",
+    code: "#",
+    demo: "#",
+  },
+  {
+    title: "SuPan",
+    image: "https://dummyimage.com/largerectangle",
+    cates: ["HTML", "CSS", "Svelte", "Git", "Heroku"],
+    description:
+      "Is an not so much UI thinking app developed in my last job in a backery, used for made request of bread from the stores to the workshop, and to plan all the bread producton includes the bread recipes and ingrediente managment. ",
+    code: "#",
+    demo: "#",
+  },
+  {
+    title: "SuPan",
+    image: "https://dummyimage.com/largerectangle",
+    cates: ["HTML", "CSS", "Svelte", "Git", "Heroku"],
+    description:
+      "Is an not so much UI thinking app developed in my last job in a backery, used for made request of bread from the stores to the workshop, and to plan all the bread producton includes the bread recipes and ingrediente managment. ",
+    code: "#",
+    demo: "#",
+  },
+];
