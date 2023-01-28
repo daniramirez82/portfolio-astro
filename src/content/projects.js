3cdb1b060db9ed48fa64658c31e693d920e932a6
Dani <dani.ramirez.madrid@gmail.com>
2023-01-28 12:56:45 +0100

    4

--- src/content/projects.js
+++ src/content/projects.js
@@ -1,12 +1,12 @@
 export const projects = [
   {
-    title: "SuPan",
-    image: "https://dummyimage.com/largerectangle",
-    cates: ["HTML", "CSS", "Svelte", "Git", "Heroku"],
+    title: "The News",
+    image: "/images/images-projects/news.png",
+    cates: ["Nextjs", "Taildwind", "AtlassianUX", "Vercel"],
     description:
-      "Is an not so much UI thinking app developed in my last job in a backery, used for made request of bread from the stores to the workshop, and to plan all the bread producton includes the bread recipes and ingrediente managment. ",
-    code: "#",
-    demo: "#",
+      "This is a personal project I used to learn Next.js. After learning a little bit of React, I proposed to myself to take advantage of the Next.js features, such as server-side rendering and lazy image loading. It is just a news site that takes the information from the New York Times API",
+    code: "https://github.com/daniramirez82/nextjs-blog",
+    demo: "http://nextjs-blog-daniramirez82.vercel.app/",
   },
   {
     title: "SuPan",
