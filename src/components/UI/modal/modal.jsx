034d2f25bec68bb6eafaa0f1b334ead5868ca3e0
daniramirez82 <dani.ramirez.madrid@gmail.com>
2023-04-21 21:23:03 +0200

    working 2

--- src/components/UI/modal/modal.jsx
+++ src/components/UI/modal/modal.jsx
@@ -1,8 +1,14 @@
-
-const Modal = ()=>{
+import ButtonMain from "../buttons/ButtonMain";
+const Modal = ({ title, message, onClick }) => {
     return (
-        <div className="bg-green-500 w-screen h-screen absolute left-0 top-0 z-50 scro " >
-jhg
+        <div className="fixed -mt-44 left-0 right-0 top-0 bottom-0  bg-white/80 backdrop-blur-sm z-50 flex justify-center items-center" >
+            <div className="bg-green-100 w-4/5 md:w-80 p-8 rounded-md shadow-lg flex flex-col">
+                <h1 className="text-2xl tracking-tight font-bold">{title}</h1>
+                <p className="text-stone-800 pt-4 pb-8">{message}</p>
+                <div className="self-end" onClick={onClick}>
+                    <ButtonMain label={"Close"} />
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/areas/ContactArea.jsx
+++ src/components/areas/ContactArea.jsx
@@ -2,7 +2,7 @@ import TextInput from "../UI/inputs/TextInput";
 import TextAreaInput from "../UI/inputs/TextAreaInput";
 import ButtonMain from "../UI/buttons/ButtonMain";
 import Send from "../UI/icons/Send";
-import { signal } from "@preact/signals";
+import { signal, useSignal } from "@preact/signals";
 import { sendNewContact } from "../../db/dataBase";
 import { validMailRegex } from "../../helpers/validators";
 import Modal from "../UI/modal/modal";
@@ -16,9 +16,15 @@ const formInitialState = {
   message: "",
   emailError: "Type a valid email",
 };
-const formState = signal(formInitialState);
+
+
 
 const ContactArea = () => {
+
+  const formState = signal(formInitialState);
+
+  const showModal = useSignal(false);
+
   const handleInputChange = (e) => {
 
     const { value, name } = e.target;
@@ -34,63 +40,74 @@ const ContactArea = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const response = await sendNewContact(formState.value);
+
+    if (response) {
+      showModal.value = true;
+    }
+
     formState.value = formInitialState;
   };
+
+  const closeModal = ()=>{
+    showModal.value = false;
+  }
+
   return (
-    <div class="flex justify-center w-full mb-40" id="Contact">
-      <div class="lg:basis-1/4"></div>
-      <div class="mt-10 lg:basis-2/4 flex flex-col w-full max-w-md">
-        <h3 class="text-3xl w-full pl-2 mt-2 mb-4 tracking-tighter text-stone-900 font-bold mx-auto items-start">
-          I'm ready to talk...
-        </h3>
-        <form class="relative">
-          <p>{formState.value.name}</p>
-          <p>{formState.value.email}</p>
-          <p>{formState.value.emailError}</p>
-          <p>{formState.value.message}</p>
-          <TextInput
-            name="name"
-            type="text"
-            label="Name"
-            value={formState.value.name}
-            onInput={handleInputChange}
-          />
-          <TextInput
-            name="email"
-            type="email"
-            label="Email"
-            value={formState.value.email}
-            onInput={handleInputChange}
-          />
-          <small>{formState.value.emailError}</small>
-          <TextAreaInput
-            label="Message"
-            name="message"
-            placeholder=""
-            rows={5}
-            value={formState.value.message}
-            onInput={handleInputChange}
-          />
-        </form>
-        {/* <div>
+    <>
+      { showModal.value && <Modal title={"Thank you!."} message={"I will contact you back as soon as I receive this message!"} onClick={() => showModal.value = false } />}
+      <div class="flex justify-center w-full mb-40" id="Contact">
+        <div class="lg:basis-1/4"></div>
+        <div class="mt-10 lg:basis-2/4 flex flex-col w-full max-w-md">
+          <h3 class="text-3xl w-full pl-2 mt-2 mb-4 tracking-tighter text-stone-900 font-bold mx-auto items-start">
+            I'm ready to talk...
+          </h3>
+          <form class="relative">
+            <p>{formState.value.name}</p>
+            <p>{formState.value.email}</p>
+            <p>{formState.value.emailError}</p>
+            <p>{formState.value.message}</p>
+            <TextInput
+              name="name"
+              type="text"
+              label="Name"
+              value={formState.value.name}
+              onInput={handleInputChange}
+            />
+            <TextInput
+              name="email"
+              type="email"
+              label="Email"
+              value={formState.value.email}
+              onInput={handleInputChange}
+            />
+            <small>{formState.value.emailError}</small>
+            <TextAreaInput
+              label="Message"
+              name="message"
+              placeholder=""
+              rows={5}
+              value={formState.value.message}
+              onInput={handleInputChange}
+            />
+          </form>
+          {/* <div>
         <div class="g-recaptcha" data-sitekey="6Lc53I0lAAAAAC9Pw8V4412fhTs_PZxekH8UEOlL"></div>
         </div> */}
-        <div class="w-fit self-end pr-2" onClick={handleSubmit}>
-          <ButtonMain label="Send">
-            <Send />
-          </ButtonMain>
+          <div class="w-fit self-end pr-2" onClick={handleSubmit}>
+            <ButtonMain label="Send">
+              <Send />
+            </ButtonMain>
+          </div>
+        </div>
+        <div class="lg:basis-1/4">
+          <img
+            class="hidden lg:block lg:mt-28"
+            src="/images/hand2.png"
+            alt="hand"
+          />
         </div>
       </div>
-      <div class="lg:basis-1/4">
-        <img
-          class="hidden lg:block lg:mt-28"
-          src="/images/hand2.png"
-          alt="hand"
-        />
-      </div>
-      <Modal />
-    </div>
-    
+    </>
   );
 };
 
